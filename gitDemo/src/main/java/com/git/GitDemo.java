b6b6db462ea76c368ea817d71dab9add3f5849d0
xiefn <17311357311@qq.com>
2018-03-07 14:08:29 +0800

    san

--- gitDemo/src/main/java/com/git/GitDemo.java
+++ gitDemo/src/main/java/com/git/GitDemo.java
@@ -5,5 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("第一次使用");
 		System.out.println("第二次使用");
+		System.out.println("第sen次使用");
+		System.out.println("第si次使用");
 	}
 }
